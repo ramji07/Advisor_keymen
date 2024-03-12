cb9441efa0705d4fe509d165b2ca0a459ddd8c9f
RamjeeC <112837025+RamjeeC@users.noreply.github.com>
2024-03-12 12:59:19 +0530

    build

--- server/server.js
+++ server/server.js
@@ -5,13 +5,11 @@ import Routes from './Routes/user.Routes.js'
 import cors from 'cors'
 const app = express()
 dotenv.config()
-import path from 'path'
+
 
 
 app.use(express.json())
-const _dirname = path.dirname('')
-const buildPath = path.join(_dirname, '../client/build')
-app.use(express.static(buildPath))
+
 app.use(cors())
 app.use( '/',Routes)
 
